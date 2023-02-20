ceb518c6734901b034c51010fb4107da0a49ab47
Vladislav Rudakov <vladislav.rudakoff@gmail.com>
2023-02-21 00:08:18 +0300

    Add light

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::cmp;
 
 use tcod::colors::*;
 use tcod::console::*;
+use tcod::map::{FovAlgorithm, Map as FovMap};
 
 use rand::Rng;
 
@@ -15,11 +16,26 @@ const ROOM_MAX_SIZE: i32 = 10;
 const ROOM_MIN_SIZE: i32 = 6;
 const MAX_ROOMS: i32 = 30;
 
-const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const FOV_ALGO: FovAlgorithm = FovAlgorithm::Basic; // default FOV algorithm
+const FOV_LIGHT_WALLS: bool = true; // light walls or not
+const TORCH_RADIUS: i32 = 10;
+
+const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 0 };
+const COLOR_LIGHT_WALL: Color = Color {
+    r: 130,
+    g: 110,
+    b: 50,
+};
+
 const COLOR_DARK_GROUND: Color = Color {
-    r: 50,
-    g: 50,
-    b: 150,
+    r: 49,
+    g: 46,
+    b: 94,
+};
+const COLOR_LIGHT_GROUND: Color = Color {
+    r: 200,
+    g: 180,
+    b: 50,
 };
 
 const LIMIT_FPS: i32 = 30;
@@ -29,6 +45,7 @@ type Map = Vec<Vec<Tile>>;
 struct Tcod {
     root: Root,
     con: Offscreen,
+    fov: FovMap,
 }
 
 #[derive(Debug)]
@@ -208,12 +225,14 @@ fn main() {
     .title("Rust/libtcod tutorial")
     .init();
 
-    let con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
-
-    let mut tcod = Tcod { root, con };
+    let mut tcod = Tcod { 
+        root,
+        con: Offscreen::new(MAP_WIDTH, MAP_HEIGHT),  
+        fov: FovMap::new(MAP_WIDTH, MAP_HEIGHT), 
+    };
 
     // create object representing the player
-    let player = Object::new(0, 0, '@', DARK_YELLOW);
+    let player = Object::new(0, 0, '@', DARKER_BLUE);
 
     // the list of objects with those two
     let mut objects = [player];
@@ -222,14 +241,29 @@ fn main() {
         map: make_map(&mut objects[0])
     };
 
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            tcod.fov.set(
+                x,
+                y,
+                !game.map[x as usize][y as usize].block_sight,
+                !game.map[x as usize][y as usize].blocked,
+            );
+        }
+    }
+
+    let mut previous_player_position = (-1, -1);
+
     while !tcod.root.window_closed() {
         tcod.con.clear();
         
-        render_all(&mut tcod, &game, &objects);
+        let fov_recompute = previous_player_position != (objects[0].x, objects[0].y);
+        render_all(&mut tcod, &game, &objects, fov_recompute);
 
         tcod.root.flush();
 
         let player = &mut objects[0];
+        previous_player_position = (player.x, player.y);
         let exit = handle_keys(&mut tcod, &game, player);
 
         if exit {
@@ -259,17 +293,27 @@ fn handle_keys(tcod: &mut Tcod, game: &Game, player: &mut Object) -> bool {
     false
 }
 
-fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
+fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object], fov_recompute: bool) {
+    if fov_recompute {
+        // recompute FOV if needed (the player moved or something)
+        let player = &objects[0];
+        tcod.fov
+            .compute_fov(player.x, player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGO);
+    }
     for y in 0..MAP_HEIGHT {
         for x in 0..MAP_WIDTH {
+            let visible = tcod.fov.is_in_fov(x, y);
             let wall = game.map[x as usize][y as usize].block_sight;
-            if wall {
-                tcod.con
-                    .set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
-            } else {
-                tcod.con
-                    .set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
-            }
+            let color = match (visible, wall) {
+                // outside of field of view:
+                (false, true) => COLOR_DARK_WALL,
+                (false, false) => COLOR_DARK_GROUND,
+                // inside fov:
+                (true, true) => COLOR_LIGHT_WALL,
+                (true, false) => COLOR_LIGHT_GROUND,
+            };
+            tcod.con
+                .set_char_background(x, y, color, BackgroundFlag::Set);
         }
     }
 
